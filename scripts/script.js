2a7f9489ab74ecac7b28915f28f66ec2db1e3a09
tomvanderputten <145463436+tomvanderputten@users.noreply.github.com>
2024-09-25 11:32:34 +0200

    hoi12121212

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+var openButton = document.querySelector("header > a")
+
+openButton.onclick = openMenu
+
+function openMenu() {
+    var deNav = document.querySelector("nav");
+    deNav.classList.add("toonMenu");
+  }
