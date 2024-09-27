d2ba22f53b721d829297491093791f467378dd05
tomvanderputten <145463436+tomvanderputten@users.noreply.github.com>
2024-09-27 11:29:01 +0200

    hoi34124124

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,39 @@
-// JavaScript Document
-console.log("test");
+/******************************/
+/* menu openen de MENU button */
+/******************************/
 
+/* JOUW CODE HIER - stap 4 */
 
-//codepen van hamburger menu gebruikt als inspiratie https://codepen.io/tomvanderputten/pen/RwzmZgJ
-var openButton = document.querySelector(".hamburgericoon")
+// stap 1: zoek de menu-button op en sla die op in een variabele
+var openButton = document.querySelector("header > button")
 
-var deNav = document.querySelector("header nav ul")
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+openButton.onclick = openMenu
 
-openButton.onclick = function() {
-    deNav.classList.toggle("toonMenu")
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.add("toonMenu");
 }
+
+
+
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+/* JOUW CODE HIER - stap 5 */
+
+// stap 1 - zoek sluiten button op
+var sluitButton = document.querySelector("nav button")
+
+// stap 2 - laat die button luisteren naar kliks
+sluitButton.onclick = sluitMenu
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu(){
+  var deNav = document.querySelector("nav")
+  deNav.classList.remove("toonMenu")
+}
+
