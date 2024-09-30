101f83cb8b3c8b0152f02b6d6cfdc13760fe6a04
tomvanderputten <145463436+tomvanderputten@users.noreply.github.com>
2024-09-30 14:00:43 +0200

    hamburgerv2

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@
 /* JOUW CODE HIER - stap 4 */
 
 // stap 1: zoek de menu-button op en sla die op in een variabele
-var openButton = document.querySelector("header > button")
+var openButton = document.querySelector("header > button:nth-of-type(2)")
 
 // stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 openButton.onclick = openMenu
@@ -26,7 +26,7 @@ function openMenu() {
 /* JOUW CODE HIER - stap 5 */
 
 // stap 1 - zoek sluiten button op
-var sluitButton = document.querySelector("nav button")
+var sluitButton = document.querySelector("nav button:nth-of-type(2)")
 
 // stap 2 - laat die button luisteren naar kliks
 sluitButton.onclick = sluitMenu
