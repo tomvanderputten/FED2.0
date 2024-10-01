4068fdf0dbbbf866c9d0b288feaac74ddc83ca79
tomvanderputten <145463436+tomvanderputten@users.noreply.github.com>
2024-10-01 20:30:16 +0200

    1 oktober

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,8 @@
-/******************************/
-/* menu openen de MENU button */
-/******************************/
-
-/* JOUW CODE HIER - stap 4 */
-
-// stap 1: zoek de menu-button op en sla die op in een variabele
+/* bron: mijn eigen codepen opdracht van de hamburger menu */
 var openButton = document.querySelector("header > button:nth-of-type(2)")
 
-// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
 openButton.onclick = openMenu
 
-// stap 3: voeg in de functie een class toe aan de nav
 function openMenu() {
   var deNav = document.querySelector("nav");
   deNav.classList.add("toonMenu");
@@ -19,19 +11,12 @@ function openMenu() {
 
 
 
-/************************************/
-/* menu sluiten met de sluit button */
-/************************************/
 
-/* JOUW CODE HIER - stap 5 */
 
-// stap 1 - zoek sluiten button op
 var sluitButton = document.querySelector("nav button:nth-of-type(2)")
 
-// stap 2 - laat die button luisteren naar kliks
 sluitButton.onclick = sluitMenu
 
-// stap 3 - in de functie verwijder de class van de nav
 function sluitMenu(){
   var deNav = document.querySelector("nav")
   deNav.classList.remove("toonMenu")
